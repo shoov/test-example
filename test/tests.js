d8c5b90dc12346a6556b193e608ca8c14feed24e
Amitai Burstein <amitai@gizra.com>
2015-04-27 23:02:02 +0300

    Add caps config example

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,29 @@
 
 var shoovWebdrivercss = require('shoov-webdrivercss');
 
-var testName = 'chrome';
+// This is an example assuming BrowserStack is used, as the capabilites is
+// encoded in the way their platform is using.
+// See https://www.browserstack.com/automate/node
+var capsConfig = {
+  'chrome': {
+    'browser' : 'Chrome',
+    'browser_version' : '39.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'resolution' : '1024x768'
+  },
+  'ie11': {
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1024x768'
+  }
+}
+
+var selectedCaps = process.env.SELECTED_CAPS;
+var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
+var testName = selectedCaps || 'default';
 
 describe('Search engine tests', function() {
 
@@ -10,7 +32,7 @@ describe('Search engine tests', function() {
   var client = {};
 
   before(function(done){
-    client = shoovWebdrivercss.before(done);
+    client = shoovWebdrivercss.before(done, caps);
   });
 
   after(function(done) {
