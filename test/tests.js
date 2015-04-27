60b4538c7063f557f2925b067d66fc95bc8a531a
Amitai Burstein <amitai@gizra.com>
2015-04-27 23:26:16 +0300

    Give example for multiple capabilities

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,35 @@
 
 var shoovWebdrivercss = require('shoov-webdrivercss');
 
-var testName = 'chrome';
+// This is an example assuming BrowserStack is used, as the capabilities are
+// encoded in the way their system is using.
+// See https://www.browserstack.com/automate/node
+
+// This can be executed by passing the environment argument like this:
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=ie11 mocha
+// PROVIDER_PREFIX=browserstack SELECTED_CAPS=chrome mocha
+var capsConfig = {
+  'chrome': {
+    'browser' : 'Chrome',
+    'browser_version' : '39.0',
+    'os' : 'OS X',
+    'os_version' : 'Yosemite',
+    'resolution' : '1024x768'
+  },
+  'ie11': {
+    'browser' : 'IE',
+    'browser_version' : '11.0',
+    'os' : 'Windows',
+    'os_version' : '7',
+    'resolution' : '1024x768'
+  }
+}
+
+var selectedCaps = process.env.SELECTED_CAPS;
+var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
+
+var providerPrefix = process.env.PROVIDER_PREFIX || '';
+var testName = providerPrefix + '-' + selectedCaps || providerPrefix + '-' + 'default';
 
 describe('Search engine tests', function() {
 
@@ -10,7 +38,7 @@ describe('Search engine tests', function() {
   var client = {};
 
   before(function(done){
-    client = shoovWebdrivercss.before(done);
+    client = shoovWebdrivercss.before(done, caps);
   });
 
   after(function(done) {
